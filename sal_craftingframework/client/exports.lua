f274071e9690520142101267f053384628a9bacf
VYKE <vyke.official@gmail.com>
2026-01-08 10:57:08 +0100

    1

--- sal_craftingframework/client/exports.lua
+++
@@ -1,9 +0,0 @@
-local State = require('client/state')
-
-exports('openCraftingUI', function()
-  TriggerServerEvent('sal_crafting:server:open', {})
-end)
-
-exports('isCraftingOpen', function()
-  return State.IsOpen()
-end)
