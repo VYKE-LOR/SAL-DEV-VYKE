8214778ab7bf0ab78a7487b1996dce10dc754aa0
VYKE <134619351+VYKE-LOR@users.noreply.github.com>
2026-01-09 10:07:15 +0100

    Restore separate audio track

---
+++ deathscreen-ui/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+loadscreen 'html/index.html'
+loadscreen_cursor 'yes'
+loadscreen_manual_shutdown 'yes'
+
+files {
+  'html/index.html',
+  'html/styles.css',
+  'html/script.js',
+  'html/assets/video/background.mp4',
+  'html/assets/audio/loading.ogg'
+}
+
+client_scripts {
+  'client.lua'
+}
