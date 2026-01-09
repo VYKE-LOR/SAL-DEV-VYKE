fb33723ff61938bdd108cb45452b54514ce87fe7
VYKE <134619351+VYKE-LOR@users.noreply.github.com>
2026-01-09 09:52:50 +0100

    Retry unmuting video audio

---
+++ deathscreen-ui/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+loadscreen 'html/index.html'
+loadscreen_cursor 'yes'
+loadscreen_manual_shutdown 'yes'
+
+files {
+  'html/index.html',
+  'html/styles.css',
+  'html/script.js',
+  'html/assets/video/background.mp4'
+}
+
+client_scripts {
+  'client.lua'
+}
