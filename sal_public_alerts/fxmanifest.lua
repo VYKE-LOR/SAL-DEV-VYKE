1916459335219ffa57679ef50e864ed8113dd231
VYKE <134619351+VYKE-LOR@users.noreply.github.com>
2026-01-15 16:25:31 +0100

    Improve critical alert UX and send flow

---
+++ sal_public_alerts/fxmanifest.lua
@@ -0,0 +1,42 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+name 'sal_public_alerts'
+author 'SAL'
+description 'DESPS Public Alert lb-phone app'
+version '1.0.0'
+
+shared_scripts {
+    'shared/config.lua',
+    'shared/locales/de.lua'
+}
+
+client_scripts {
+    'client/cl_main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/sv_db.lua',
+    'server/sv_main.lua'
+}
+
+files {
+    'ui/index.html',
+    'ui/styles.css',
+    'ui/app.js',
+    'ui/**/*'
+}
+
+dependencies {
+    'lb-phone',
+    'es_extended',
+    'oxmysql'
+}
+
+optional_dependencies {
+    'lb-nativeaudio',
+    'xsound'
+}
